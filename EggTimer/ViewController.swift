9b1f42bf935c7c7ad8d6992233e24f1402ec6e85
Gi Pyo Kim <gipyok@gmail.com>
2021-09-17 22:00:11 -0700

    Refactored the timer to a single variable so that only one timer can be triggered at a time. Added progress view.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,19 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes: [String: Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes: [String: Int] = ["Soft": 1, "Medium": 2, "Hard": 3]
+    var timer = Timer()
         
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
         
         guard let hardness = sender.currentTitle else { return }
         
         var timeRemaining = eggTimes[hardness]!
         
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
             if timeRemaining > 0 {
                 print("\(timeRemaining) seconds")
                 timeRemaining -= 1
             } else {
+                self.titleLabel.text = "\(hardness) egg done"
                 timer.invalidate()
             }
         }
